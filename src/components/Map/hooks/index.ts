54e09ba6879c4f70dc015c37fe6e1747c8d06c1e
Lyes Tarzalt <lyes.trzlt@gmail.com>
2026-02-07 02:52:55 +0800

    feat(explore): wire up route selection to map layer

--- src/components/Map/hooks/index.ts
+++ src/components/Map/hooks/index.ts
@@ -3,3 +3,4 @@ export { useMapSetup, type MapRef, type PopupRef } from './useMapSetup';
 export { useNavLayerSync, applyNavVisibilityChange } from './useNavLayerSync';
 export { useVatsimSync, toggleVatsimLayer } from './useVatsimSync';
 export { useAirportInteractions } from './useAirportInteractions';
+export { useRouteLineSync } from './useRouteLineSync';
