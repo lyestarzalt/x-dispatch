87498ae75c90d6397ea8c94dcd181862a8c3a113
Lyes Tarzalt <lyes.trzlt@gmail.com>
2026-02-22 16:35:18 +0800

    chore: update Sentry config with env vars for org/project

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -7,7 +7,7 @@ import './index.css';
 
 sentryInit(
   {
-    dsn: 'https://0279f306474c382f68b1605fb27be652@o4508345478742016.ingest.de.sentry.io/4510878234837072',
+    dsn: 'https://93939f3ad736f402a616188303a369cf@o4510928623173632.ingest.de.sentry.io/4510928631365712',
   },
   reactInit
 );
